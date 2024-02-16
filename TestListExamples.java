11047aa11850ac37999b96139a867362a7c1ea06
Sa-Rangaraj <srangaraj@ucsd>
2024-02-15 22:07:24 -0800

    Created autograder script, and added test cases

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+
+import org.hamcrest.core.Is;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
@@ -17,5 +19,74 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(right, left);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  } 
+
+
+  @Test(timeout = 500)
+  public void testMergeNullArg() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    assertThrows( IllegalArgumentException.class, () -> {ListExamples.merge(null, left);});
+    assertThrows( IllegalArgumentException.class, () -> {ListExamples.merge(right, null);});
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> list = Arrays.asList("a", "moon", "moon beam");
+    List<String> expected = Arrays.asList("moon");
+    List<String> filtered = ListExamples.filter(list, new IsMoon());
+    assertEquals(expected, filtered);
+  } 
+
+  // @Test(timeout = 500)
+  // public void testFilterSubset() {
+  //   List<String> list = Arrays.asList("a", "b", "c", "d", "e");
+  //   List<String> filter = Arrays.asList("b", "d", "f", "g");
+  //   List<String> expected = Arrays.asList("b", "d");
+  //   List<String> filtered = ListExamples.filter(list, new IsMoon());
+  //   assertEquals(expected, filtered);
+  // } 
+
+  // @Test(timeout = 500)
+  // public void testFilterSameFilterDiffOrder() {
+  //   List<String> list = Arrays.asList("a", "b", "c");
+  //   List<String> filter = Arrays.asList("b", "c", "a");
+  //   List<String> expected = Arrays.asList("a", "b", "c");
+  //   List<String> filtered = ListExamples.filter(list, filter);
+  //   assertEquals(expected, filtered);
+  // } 
+
+
+  // @Test(timeout = 500)
+  // public void testFilterSameListDiffOrder() {
+  //   List<String> filter = Arrays.asList("a", "b", "c");
+  //   List<String> list = Arrays.asList("b", "c", "a");
+  //   List<String> expected = Arrays.asList("a", "b", "c");
+  //   List<String> filtered = ListExamples.filter(list, filter);
+  //   assertEquals(expected, filtered);
+  // } 
+
+  // @Test(timeout = 500)
+  // public void testFilterNoSame() {
+  //   List<String> filter = Arrays.asList("a", "b", "c");
+  //   List<String> list = Arrays.asList("d", "e", "f");
+  //   List<String> filtered = ListExamples.filter(list, filter);
+  //   assertTrue(filtered.isEmpty());
+  // }
+  
+  @Test(timeout = 500)
+  public void testFilterNullArg() {
+    List<String> list = Arrays.asList("a", "b", "c");
+    assertThrows( IllegalArgumentException.class, () -> {ListExamples.filter(null, new IsMoon());});
+    assertThrows( IllegalArgumentException.class, () -> {ListExamples.filter(list, null);});
   }
 }
--- grade.sh
+++ grade.sh
@@ -5,8 +5,18 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
-echo 'Finished cloning'
+git clone $1 student-submission 2> /dev/null
+
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+    cp -r lib grading-area/
+else
+    echo "File not found: ListExamples.java"
+    exit 1
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
@@ -14,3 +24,33 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cd grading-area
+
+javac -cp $CPATH *.java  
+
+
+
+if [[ $? -ne 0 ]]
+then
+  echo "Program failed to compile, compiler error shown above"
+  exit 1
+fi
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+
+
+if [[ $lastline = *OK* ]]
+then
+    totalTests=$(echo $lastline | awk -F'[ (]' '{print $3}')
+    echo "Your score is: $totalTests/$totalTests"
+else
+    totalTests=$(echo $lastline | awk -F'[ ,]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[ ,]' '{print $6}')
+    successes=$((totalTests - failures))
+    echo "Your score is: $successes/$totalTests"
+fi
